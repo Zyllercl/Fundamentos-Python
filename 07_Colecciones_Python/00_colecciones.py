57843cfd95df03df10eab87339c3176641b195a0
Martin Villa <mvilla@kdoce.cl>
2024-12-20 16:53:07 -0300

    Comenzando colecciones en python

---
+++ 07_Colecciones_Python/00_colecciones.py
@@ -0,0 +1,18 @@
+'''
+    COLECIONES EN PYTHON (LISTAS - TUPLAS - DICCIONARIOS)
+
+    Def:
+        - Una colección es un conjunto de datos. En Python existen varios tipos que podemos utilizar con el objetivo de almacenar, organizar y manipular múltiples conjuntos de datos, esto se conoce como Estructura de Datos.
+
+        - Tipos de Estructuras de Datos:
+        
+            1.- Listas
+                
+                Def:
+                    - Las listas son coleciones ordenadas y mutables (puede varias) 
+
+
+            2.- Tuplas
+            3.- Set (conjunto)
+            4.- Diccionario
+'''
