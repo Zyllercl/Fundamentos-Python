1cdc602f1a8e462d298b6fe20b9eefc5aef12795
Martin Villa <mvilla@kdoce.cl>
2025-08-20 10:21:21 -0400

    Modificacion de texto

--- 05_Sentencias_Control_Python/Ejercicios/03_reto_casa_embrujada.py
+++ 05_Sentencias_Control_Python/Ejercicios/03_reto_casa_embrujada.py
@@ -7,8 +7,8 @@ Definición:
 
 Crear un programa que determine si un usuario puede ingresar a una casa embrujada en Fantasilandia. Para ello, se deben cumplir las siguientes condiciones:
 
-1. El usuario debe tener más de 10 años.
-2. El usuario no debe tener miedo a la oscuridad.
+    1. El usuario debe tener más de 10 años.
+    2. El usuario no debe tener miedo a la oscuridad.
 
 Si se cumplen ambas condiciones, el usuario puede ingresar; de lo contrario, no puede ingresar.
 """
